36e9925f9790fef21f7e66face743b3887c829d6
Ghataorhe <cssb-ghataorhe@Ghataorhes-MacBook-Pro.local>
2018-03-31 13:21:42 -0700

    fixes numberOfImages state

--- photo-carousel/src/components/ProgressComponent.js
+++ photo-carousel/src/components/ProgressComponent.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 //stateless export
 export const ProgressComponent = (props) => {
-    const {currentIndex, numberOfimages} =  props;
+    const {currentIndex, numberOfImages} =  props;
     return (
         <div>
-            <p>You are viewing {currentIndex} of {numberOfimages}</p>
+            <p>You are viewing {currentIndex} of {numberOfImages}</p>
         </div>
     )
 }
